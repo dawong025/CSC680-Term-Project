a7107c5ea491487b689947bf18ce9302da67423d
dawong025 <dwong19@mail.sfsu.edu>
2022-11-25 00:41:23 -0800

    Adding base proj

---
+++ CSC680 Term Project/CSC680 Term Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CSC680 Term Project
+//
+//  Created by Darren Wong on 11/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
